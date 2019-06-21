906ae2a163bb8d533ed59d6c6e5558764d6b014f
celineduflo <cduflo@hoytmail.com>
2019-06-21 17:07:20 +0200

    1st commit

--- Test1/ViewController.swift
+++ Test1/ViewController.swift
@@ -9,12 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+   
+    @IBOutlet weak var activity: UIActivityIndicatorView!
+    @IBOutlet weak var switchState: UISwitch!
+    @IBOutlet weak var progress: UIProgressView!
+    @IBOutlet weak var stepState: UIStepper!
+    @IBOutlet weak var segmentState: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
 
-
+    @IBAction func switchh(_ sender: UISwitch) {
+        animateActivity()
+    }
+    
+    @IBAction func stepper(_ sender: UIStepper) {
+        animateProgress()
+    }
+    @IBAction func segment(_ sender: UISegmentedControl) {
+        changeColor()
+    }
+    
+    func animateActivity() {
+        if switchState.isOn {
+           activity.startAnimating()
+        } else {
+           activity.stopAnimating()
+        }
+    }
+    
+    func animateProgress() {
+        if switchState.isOn {
+            progress.progress = Float(stepState.value)/10
+        } else {
+            activity.stopAnimating()
+        }
+    }
+    
+    func changeColor() {
+        
+    }
+    
+    
 }
 
